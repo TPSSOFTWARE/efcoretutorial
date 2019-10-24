c140003693f1017386f8bfa8dbe7550da5946e39
Nertil Poci <nertilpoci@hotmail.com>
2019-10-24 06:17:51 +0200

    Install & Configure

---
+++ EFCoreTutorial/EFCoreTutorial/Controllers/EFController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCoreTutorial.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EFCoreTutorial.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EFController : ControllerBase
+    {
+        EfCoreContext _efCoreContext;
+        public EFController(EfCoreContext efCoreContext)
+        {
+            _efCoreContext = efCoreContext;
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public IActionResult CreateDb()
+        {
+            _efCoreContext.Database.EnsureCreated();
+            return Ok();
+        }
+    }
+}
---
+++ EFCoreTutorial/EFCoreTutorial/DAL/EfCoreContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCoreTutorial.DAL
+{
+    public class EfCoreContext:DbContext
+    {
+       public EfCoreContext(DbContextOptions<EfCoreContext> options) : base(options)
+        {
+
+        }
+    }
+}
